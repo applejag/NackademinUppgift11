b44a83f9be79e1b3d9adc0394f86686c52047dfd
kalle (jag) <kalle.jillheden@gmail.com>
2018-10-23 14:24:06 +0200

    Added RGBCode model and DataBindings to the model

---
+++ ColorPicker/Models/RGBCode.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ColorPicker.Annotations;
+
+namespace ColorPicker.Models
+{
+    public class RGBCode : INotifyPropertyChanged
+    {
+        private byte _blue = 255;
+        private byte _green = 255;
+        private byte _red = 255;
+
+        public byte Red
+        {
+            get => _red;
+            set { _red = value; OnPropertyChanged(); }
+        }
+
+        public byte Green
+        {
+            get => _green;
+            set { _green = value; OnPropertyChanged(); }
+        }
+
+        public byte Blue
+        {
+            get => _blue;
+            set { _blue = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ColorPicker/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ColorPicker.Annotations;
+using ColorPicker.Models;
+
+namespace ColorPicker.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public RGBCode RgbCode { get; } = new RGBCode();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            RgbCode.PropertyChanged += RgbCodeOnPropertyChanged;
+        }
+
+        private void RgbCodeOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(RgbCode));
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
